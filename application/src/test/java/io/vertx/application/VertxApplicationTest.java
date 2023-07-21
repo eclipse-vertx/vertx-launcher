5bf7f629568ce19065c06bbf8078d95040d6d6af
Thomas Segismont <tsegismont@gmail.com>
2023-07-22 00:24:55 +0200

    Add more tests in VertxApplicationTest
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- application/src/test/java/io/vertx/application/VertxApplicationTest.java
+++ application/src/test/java/io/vertx/application/VertxApplicationTest.java
@@ -11,27 +11,35 @@
 
 package io.vertx.application;
 
+import io.vertx.core.CustomMetricsOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.application.HookContext;
 import io.vertx.core.application.VertxApplication;
 import io.vertx.core.application.VertxApplicationHooks;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.metrics.MetricsOptions;
+import io.vertx.core.spi.VertxServiceProvider;
 import io.vertx.test.fakecluster.FakeClusterManager;
+import io.vertx.test.verticles.TestVerticle;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static io.vertx.core.application.ExitCodes.VERTX_DEPLOYMENT;
@@ -50,8 +58,14 @@ public class VertxApplicationTest {
   private ByteArrayOutputStream out;
   private ByteArrayOutputStream err;
 
+  @BeforeEach
+  void setUp() {
+    TestVerticle.instanceCount.set(0);
+    TestVerticle.conf = null;
+  }
+
   @AfterEach
-  public void tearDown() throws IOException {
+  void tearDown() throws IOException {
     if (manifest != null) {
       Files.deleteIfExists(manifest);
     }
@@ -174,17 +188,201 @@ public class VertxApplicationTest {
         metricsOptions.set(context.vertxOptions().getMetricsOptions());
       }
     };
+    ClassLoader oldCL = Thread.currentThread().getContextClassLoader();
+    Thread.currentThread().setContextClassLoader(createMetricsFromMetaInfLoader("io.vertx.core.CustomMetricsFactory"));
     try {
       System.setProperty("vertx.metrics.options.enabled", "true");
       myVertxApplication.launch(new String[0], hooks);
     } finally {
-      System.clearProperty("vertx.metrics.options.enabled");
+      Thread.currentThread().setContextClassLoader(oldCL);
+      clearProperties();
     }
     assertServerStarted();
     assertNotNull(metricsOptions.get());
     assertTrue(metricsOptions.get().isEnabled());
   }
 
+  @Test
+  public void testRunVerticle() {
+    testRunVerticleMultiple(1);
+  }
+
+  @Test
+  public void testRunVerticleMultipleInstances() {
+    testRunVerticleMultiple(10);
+  }
+
+  public void testRunVerticleMultiple(int instances) {
+    VertxApplication myVertxApplication = new VertxApplication();
+    myVertxApplication.launch(new String[]{"java:" + TestVerticle.class.getCanonicalName(), "-instances", String.valueOf(instances)}, hooks);
+    await("Server not started")
+      .atMost(Duration.ofSeconds(10))
+      .until(() -> TestVerticle.instanceCount.get(), equalTo(instances));
+  }
+
+  @TempDir
+  File testFolder;
+
+  @Test
+  public void testConfigureFromJsonFile() throws Exception {
+    testConfigureFromJson(true);
+  }
+
+  @Test
+  public void testConfigureFromJsonString() throws Exception {
+    testConfigureFromJson(false);
+  }
+
+  private void testConfigureFromJson(boolean jsonFile) throws Exception {
+    JsonObject json = new JsonObject()
+      .put("eventLoopPoolSize", 123)
+      .put("maxEventLoopExecuteTime", 123767667)
+      .put("metricsOptions", new JsonObject().put("enabled", true))
+      .put("eventBusOptions", new JsonObject().put("clusterPublicHost", "mars"))
+      .put("maxEventLoopExecuteTimeUnit", "SECONDS");
+
+    String optionsArg;
+    if (jsonFile) {
+      File file = new File(testFolder, "options.json");
+      Files.write(file.toPath(), json.toBuffer().getBytes());
+      optionsArg = file.getPath();
+    } else {
+      optionsArg = json.toString();
+    }
+
+    VertxApplication myVertxApplication = new VertxApplication();
+    AtomicReference<VertxOptions> vertxOptions = new AtomicReference<>();
+    hooks = new MyHooks() {
+      @Override
+      public void beforeStartingVertx(HookContext context) {
+        vertxOptions.set(context.vertxOptions());
+      }
+    };
+    myVertxApplication.launch(new String[]{"java:" + TestVerticle.class.getCanonicalName(), "-options", optionsArg}, hooks);
+    await("Server not started")
+      .atMost(Duration.ofSeconds(10))
+      .until(() -> TestVerticle.instanceCount.get(), equalTo(1));
+
+    VertxOptions opts = vertxOptions.get();
+
+    assertEquals(123, opts.getEventLoopPoolSize(), 0);
+    assertEquals(123767667L, opts.getMaxEventLoopExecuteTime());
+    assertTrue(opts.getMetricsOptions().isEnabled());
+    assertEquals("mars", opts.getEventBusOptions().getClusterPublicHost());
+    assertEquals(TimeUnit.SECONDS, opts.getMaxEventLoopExecuteTimeUnit());
+  }
+
+  @Test
+  public void testConfigureFromSystemProperties() {
+    testConfigureFromSystemProperties(false);
+  }
+
+  @Test
+  public void testConfigureFromSystemPropertiesClustered() {
+    testConfigureFromSystemProperties(true);
+  }
+
+  private void testConfigureFromSystemProperties(boolean clustered) {
+    VertxApplication myVertxApplication = new VertxApplication();
+    String[] args;
+    if (clustered) {
+      args = new String[]{"java:" + TestVerticle.class.getCanonicalName(), "-cluster"};
+    } else {
+      args = new String[]{"java:" + TestVerticle.class.getCanonicalName()};
+    }
+
+    AtomicReference<VertxOptions> vertxOptions = new AtomicReference<>();
+    hooks = new MyHooks() {
+      @Override
+      public void beforeStartingVertx(HookContext context) {
+        vertxOptions.set(context.vertxOptions());
+      }
+    };
+
+    ClassLoader oldCL = Thread.currentThread().getContextClassLoader();
+    Thread.currentThread().setContextClassLoader(createMetricsFromMetaInfLoader("io.vertx.core.CustomMetricsFactory"));
+    try {
+      System.setProperty("vertx.options.eventLoopPoolSize", "123");
+      System.setProperty("vertx.options.maxEventLoopExecuteTime", "123767667");
+      System.setProperty("vertx.metrics.options.enabled", "true");
+      System.setProperty("vertx.options.maxEventLoopExecuteTimeUnit", "SECONDS");
+      myVertxApplication.launch(args, hooks);
+    } finally {
+      Thread.currentThread().setContextClassLoader(oldCL);
+      clearProperties();
+    }
+    await("Server not started")
+      .atMost(Duration.ofSeconds(10))
+      .until(() -> TestVerticle.instanceCount.get(), equalTo(1));
+
+
+    VertxOptions opts = vertxOptions.get();
+
+    assertEquals(123, opts.getEventLoopPoolSize(), 0);
+    assertEquals(123767667L, opts.getMaxEventLoopExecuteTime());
+    assertTrue(opts.getMetricsOptions().isEnabled());
+    assertEquals(TimeUnit.SECONDS, opts.getMaxEventLoopExecuteTimeUnit());
+  }
+
+  private void clearProperties() {
+    Set<String> toClear = new HashSet<>();
+    Enumeration<?> e = System.getProperties().propertyNames();
+    // Uhh, properties suck
+    while (e.hasMoreElements()) {
+      String propName = (String) e.nextElement();
+      if (propName.startsWith("vertx.options")) {
+        toClear.add(propName);
+      }
+    }
+    toClear.forEach(System::clearProperty);
+  }
+
+  @Test
+  public void testCustomMetricsOptions() {
+    VertxApplication myVertxApplication = new VertxApplication();
+
+    AtomicReference<VertxOptions> vertxOptions = new AtomicReference<>();
+    hooks = new MyHooks() {
+      @Override
+      public void beforeStartingVertx(HookContext context) {
+        vertxOptions.set(context.vertxOptions());
+      }
+    };
+    ClassLoader oldCL = Thread.currentThread().getContextClassLoader();
+    Thread.currentThread().setContextClassLoader(createMetricsFromMetaInfLoader("io.vertx.core.CustomMetricsFactory"));
+    try {
+      System.setProperty("vertx.metrics.options.enabled", "true");
+      System.setProperty("vertx.metrics.options.customProperty", "customPropertyValue");
+      myVertxApplication.launch(new String[]{"java:" + TestVerticle.class.getCanonicalName()}, hooks);
+    } finally {
+      Thread.currentThread().setContextClassLoader(oldCL);
+      clearProperties();
+    }
+    await("Server not started")
+      .atMost(Duration.ofSeconds(10))
+      .until(() -> TestVerticle.instanceCount.get(), equalTo(1));
+
+
+    VertxOptions opts = vertxOptions.get();
+
+    CustomMetricsOptions custom = (CustomMetricsOptions) opts.getMetricsOptions();
+    assertEquals("customPropertyValue", custom.getCustomProperty());
+    assertTrue(hooks.vertx.isMetricsEnabled());
+  }
+
+  private ClassLoader createMetricsFromMetaInfLoader(String factoryFqn) {
+    return new URLClassLoader(new URL[0], Thread.currentThread().getContextClassLoader()) {
+      @Override
+      public Enumeration<URL> findResources(String name) throws IOException {
+        if (name.equals("META-INF/services/" + VertxServiceProvider.class.getName())) {
+          File f = new File(testFolder, "vertx.txt");
+          Files.write(f.toPath(), factoryFqn.getBytes());
+          return Collections.enumeration(Collections.singleton(f.toURI().toURL()));
+        }
+        return super.findResources(name);
+      }
+    };
+  }
 
   public static class MyHooks implements VertxApplicationHooks {
 
