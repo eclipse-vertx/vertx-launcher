982cf23e72a6cdc898d469a034078329a2162835
Thomas Segismont <tsegismont@gmail.com>
2023-12-07 14:50:47 +0100

    Add JsonGen annotation (#6)
    Fixes build issue
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- cli/src/main/java/io/vertx/core/cli/Argument.java
+++ cli/src/main/java/io/vertx/core/cli/Argument.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2023 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,6 +12,7 @@
 package io.vertx.core.cli;
 
 import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.json.annotations.JsonGen;
 import io.vertx.core.json.JsonObject;
 
 import java.util.Objects;
@@ -23,7 +24,8 @@ import java.util.Objects;
  * @author Clement Escoffier <clement@apache.org>
  * @see Option
  */
-@DataObject(generateConverter = true, publicConverter = false)
+@DataObject
+@JsonGen(publicConverter = false)
 public class Argument {
 
   /**
