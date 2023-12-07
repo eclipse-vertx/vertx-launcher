15e0ef517adf2f492c9c8a5b64335ff91ee12fc0
Thomas Segismont <tsegismont@gmail.com>
2023-12-07 12:18:27 +0100

    Add JsonGen annotation
    Fixes build issue
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- cli/src/main/java/io/vertx/core/cli/Option.java
+++ cli/src/main/java/io/vertx/core/cli/Option.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2023 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,6 +12,7 @@
 package io.vertx.core.cli;
 
 import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.json.annotations.JsonGen;
 import io.vertx.core.json.JsonObject;
 
 import java.util.Objects;
@@ -26,7 +27,8 @@ import java.util.TreeSet;
  *
  * @author Clement Escoffier <clement@apache.org>
  */
-@DataObject(generateConverter = true, publicConverter = false)
+@DataObject
+@JsonGen(publicConverter = false)
 public class Option {
 
   /**
